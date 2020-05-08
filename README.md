2ccb9782eeb8ce6d6cfb85811255c6d7c7d08a77
andrevft <a.fanchini@gmail.com>
2020-05-08 19:26:32 -0300

    firs commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# AirBnB Clone :newspaper: 
+
+Project for an AirBnB clone.
+
+## Motivation
+
+Challenge launched in the Hiring Coders project at Gama Academy which consists of building an AirBnB Clone using just JS Vanilla and retrieve data from API.
+
+## Prerequisites for the project
+
+- HTML5;
+- CSS;
+- JavaScript Vanilla
+- API
+
+
+## Author
+
+- [André Terrasan](http://www.linkedin.com/in/andreterrasan)
+
+## License
+
+This project is open source and available under the [MIT License](LICENSE.md).
+
---
+++ script.js
@@ -0,0 +1,22 @@
+async function fetchData() {
+
+	const response = await fetch('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72');
+	const rooms = await response.json();
+
+	let lista = rooms.map(room => `<div class="room-card row">
+									<div class="room-div-img"><img class="room-img" src="${room.photo}" class="room-img"/></div>
+									<div class="room-description">
+									<div class="room-type">${room.property_type}</div>
+									<div class="room-name">${room.name}</div>
+									<div class="room-price"><p><strong>R$ ${room.price}</strong>/noite</p></div>
+									</div>
+									</div>
+									<hr>`);
+
+	let names = lista.toString().replace(/\,/g, "");
+
+	document.getElementById("the-rooms").innerHTML = `<div class="room-board"> ${names} <div>`;
+
+}
+
+fetchData();
