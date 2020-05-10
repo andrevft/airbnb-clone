9b47fb6c417388d93615bd624cedeed5f7294153
andrevft <a.fanchini@gmail.com>
2020-05-09 23:01:00 -0300

    version 0.2

--- script/script.js
+++ script/script.js
@@ -3,11 +3,11 @@ async function fetchData() {
 	const response = await fetch('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72');
 	const rooms = await response.json();
 
-	let lista = rooms.map(room => `<div class="room-card row">
+	let lista = rooms.map(room => `<div class="room-board-card">
 									<div class="room-div-img"><img class="room-img" src="${room.photo}" class="room-img"/></div>
 									<div class="room-description">
-									<div class="room-type">${room.property_type} <i onclick="myFunction(this)" class="fa fa-thumbs-up"></i></div>
-									<div class="room-name">${room.name}</div>
+									<div class="room-type"><p>${room.property_type}</p> <spam> <i onclick="myFunction(this)" class="far fa-heart"></i><spam></div>
+									<div class="room-name"><p>${room.name}</p></div>
 									<div class="room-price"><p><strong>R$ ${room.price}</strong>/noite</p></div>
 									</div>
 									</div>
